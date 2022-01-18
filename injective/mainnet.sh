6d0f957b44fd77acb31fca28acf606f501fb4f6d
Cheeze <cheeze@aurastake.com>
2022-01-18 16:43:48 +0800

    Updated the info on injective to be more comprehensive.

--- injective/mainnet.sh
+++ injective/mainnet.sh
@@ -1,2 +1,5 @@
 export MONIKER=AuraStake
 export CHAIN_ID=injective-1
+export INJ_VER=v1.1.1-1640627705 # Check for the latest at https://github.com/InjectiveLabs/injective-chain-releases/releases
+export VALIDATOR_KEY_NAME=aurastake2-inj
+export ORCHESTRATOR_KEY_NAME=aurastake2-orch
--- injective/testnet.sh
+++ injective/testnet.sh
@@ -1,2 +1,6 @@
 export MONIKER=CloverStake
 export CHAIN_ID=injective-888
+export INJ_VER=v0.4.17-1640702850 # Check for the latest at https://github.com/InjectiveLabs/injective-chain-releases/releases
+export VALIDATOR_KEY_NAME=cloverstake2-inj
+export ORCHESTRATOR_KEY_NAME=cloverstake2-orch
+export TESTNET_VER=40018 # Check for the latest at https://github.com/InjectiveLabs/testnet-config/tree/master/staking
